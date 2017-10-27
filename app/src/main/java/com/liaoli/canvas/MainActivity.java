0c176708ed72c3d89b334f0c6cfad8dbaf243b28
廖理 <liaoli@livestar.com>
2017-10-27 10:05:54 +0800

    api

--- app/src/main/java/com/liaoli/canvas/MainActivity.java
+++ app/src/main/java/com/liaoli/canvas/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     {
+        items.add(new PageItem(R.layout.path,"CanvasDrawPath"));
         items.add(new PageItem(R.layout.arc,"CanvasDrawArc"));
         items.add(new PageItem(R.layout.round_rect,"DrawRoundRect"));
         items.add(new PageItem(R.layout.line,"DrawLine"));
---
+++ app/src/main/java/com/liaoli/canvas/api/CanvasDrawPath.java
@@ -0,0 +1,58 @@
+package com.liaoli.canvas.api;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class CanvasDrawPath extends View {
+
+    public CanvasDrawPath(Context context) {
+        super(context);
+    }
+
+    public CanvasDrawPath(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CanvasDrawPath(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Paint paint = new Paint();
+
+        Path path = new Path();
+        RectF rectF1 = new RectF(200, 200, 500, 500);
+        path.addArc(rectF1, -225,255);
+
+        RectF rectF2 = new RectF(500, 200, 800, 500);
+        path.arcTo(rectF2, 180, 225, false);
+
+        path.lineTo(500, 700);
+
+        canvas.drawPath(path, paint);
+
+        Path path1 = new Path();
+
+        path1.lineTo(300,300);
+
+        path1.moveTo(0,200);
+
+        path1.lineTo(600,700);
+
+        canvas.drawPath(path1, paint);
+
+    }
+
+}
